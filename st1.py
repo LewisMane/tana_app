7b812ec358ccaecf5c508dfb6838a335665e56a1
LewisGIS <96820204+LewisMane-GIS@users.noreply.github.com>
2023-10-26 14:28:47 +0300

    Update 2

--- st1.py
+++ st1.py
@@ -19,11 +19,83 @@ st.markdown(hide_menu_style, unsafe_allow_html=True)
 st.image(img, width=700)
 
 st.write("""
-####         A map displaying Water Service Providers under TWWDA coverage area
+####  A map displaying projects undertaken by TWWDA
 
-*Select an option in the select box below to view your desired WSP*
+*click on a marker to view project details*
 """)
 
 HtmlFile = open("./maps/new_map1.html", 'r')
 Tana_map = HtmlFile.read()
 components.html(Tana_map, height=700)
+
+st.subheader("Map Legend 🗺")
+
+col1, col2, col3 = st.columns([2, 2, 2])
+col1.markdown("""
+<span style='color:blue;'>
+. ADB_H2O 
+</span>=
+<span style='color:brown;'>
+Africa Development Bank Water Project
+</span><br>
+<span style='color:blue;'>
+. Cc_Ws 
+</span>=
+<span style='color:brown;'>
+Crosscounty Water Supply
+</span><br>
+<span style='color:blue;'>
+. Bh_Eq 
+</span>=
+<span style='color:brown;'>
+Borehole Equipping
+</span>
+
+""",
+              unsafe_allow_html=True)
+
+col2.markdown("""
+<span style='color:blue;'>
+. ADB_Swg
+</span>=
+<span style='color:brown;'>
+Africa Development Bank Sewerage Project
+</span><br>
+<span style='color:blue;'>
+. Cc_ws 
+</span> =
+<span style='color:brown;'>
+Crosscounty Water Supply
+</span><br>
+<span style='color:blue;'>
+. Bh_Dr 
+</span>=
+<span style='color:brown;'>
+Borehole Drilling
+</span>
+
+""",
+              unsafe_allow_html=True)
+
+col3.markdown("""
+<span style='color:blue;'>
+. UHC_Prj
+</span>=
+<span style='color:brown;'>
+Universal Health Coverage Project
+</span><br>
+<span style='color:green;'>
+. Plan & Des 
+</span> =
+<span style='color:green;'>
+Planning & Design 
+</span><br>
+<span style='color:green;'>
+. other  
+</span>=
+<span style='color:green;'>
+Other Projects
+</span>
+
+""",
+              unsafe_allow_html=True)
